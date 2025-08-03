7ccbb59e4c051c32b32d3ca8b2d8bf66d6113948
K4HVH <bronercast@gmail.com>
2025-08-03 17:27:13 +1000

    More shit

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,8 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  html: {
+    title: 'K4Tech.net - Innovative solutions, quickly',
+    favicon: './src/favicon.svg',
+  },
 });
