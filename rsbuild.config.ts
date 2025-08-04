6dc24bd04a200c725e30c3add6b6c5ecb81de6af
K4HVH <bronercast@gmail.com>
2025-08-04 20:16:05 +1000

    cache busting

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
       'og:type': 'website',
       'og:url': 'https://k4tech.net',
       'og:site_name': 'K4Tech.net',
-      'og:image': '/og-image.svg',
+      'og:image': '/og-image.svg?v=1',
       'og:image:width': '1200',
       'og:image:height': '630',
       'og:image:alt': 'K4Tech.net - Innovative solutions, quickly',
@@ -27,7 +27,7 @@ export default defineConfig({
       'twitter:card': 'summary_large_image',
       'twitter:title': 'K4Tech.net - Innovative solutions, quickly',
       'twitter:description': 'K4Tech.net - Innovative solutions, quickly. Access our services including GitHub, Discord, Webmail, Cloud storage, and Password Manager.',
-      'twitter:image': '/og-image.svg',
+      'twitter:image': '/og-image.svg?v=1',
       'twitter:image:alt': 'K4Tech.net - Innovative solutions, quickly',
       
       // Additional meta tags
